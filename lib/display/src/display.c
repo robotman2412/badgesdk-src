cf8aa7722807b7ea70018d488446443b2414e018
RobotMan2412 <julian@scheffers.net>
2023-05-13 20:53:37 +0200

    Add a new display API

---
+++ lib/display/src/display.c
@@ -0,0 +1,53 @@
+/*
+	MIT License
+
+	Copyright    (c) 2023 Julian Scheffers
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files    (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#include "display.h"
+
+// Get the amount of connected displays.
+// May be zero on badges without displays.
+int display_count() {
+	return 0;
+}
+
+// Get the width in pixels of a display.
+int display_width(int display) {
+	return 0;
+}
+// Get the height in pixels of a display.
+int display_height(int display) {
+	return 0;
+}
+
+// Set the display backlight value 0-255.
+bool display_backlight(int display, uint8_t value) {
+	return false;
+}
+// Draw the full area of a display.
+bool display_write(int display, const void *buf, size_t len) {
+	return false;
+}
+// Draw a part of the display.
+bool display_write_partial(int display, const void *buf, size_t len, int x, int y, int width, int height) {
+	return false;
+}
